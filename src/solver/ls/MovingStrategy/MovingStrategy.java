c839d5e1f047d7716f9bede2e448e8157c068fea
Amit Reddy <jsammureddy140804@gmail.com>
2025-01-27 08:33:24 +0530

    moving idea 2

--- src/solver/ls/MovingStrategy/MovingStrategy.java
+++ src/solver/ls/MovingStrategy/MovingStrategy.java
@@ -16,3 +16,20 @@ public interface MovingStrategy {
             neighborhood.add(getSingleNeighbor(currentSolution, instance));
         return neighborhood;
     }
+
+    default boolean isRouteFeasible(List<Integer> route, VRPLocalSearch instance) {
+        // the route should begin and end at the depot (and have at least 2 elements)
+        if (route.size() < 2)
+            return false;
+        if (route.get(0) != 0 || route.get(route.size() - 1) != 0)
+            return false;
+
+        // no customer in the route is visited more than once
+        int[] seenCustomers = new int[instance.getNumCustomers()];
+        for (int i = 1; i < route.size() - 1; i++) {
+            int customer = route.get(i);
+            if (seenCustomers[customer] == 1)
+                return false;
+            seenCustomers[customer] = 1;
+        }
+        
